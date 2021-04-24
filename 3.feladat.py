e1a3b57bcd0c15de3c89ff0ebefa75cb5fc498bf
Tarcsa Attila <79060616+Tariati@users.noreply.github.com>
2021-04-24 16:57:18 +0200

    Update 3.feladat.py

--- 3.feladat.py
+++ 3.feladat.py
@@ -1,22 +1,21 @@
-import matplotlib.pyplot as plt
 import matplotlib.image as image
-import random
+import numpy as np
 
 primls = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
-211, 223, 227, 229, 233, 239, 241, 251,]
-img=image.imread("flowers.jpg")
-plt.imshow()
-plt.show()
+211, 223, 227, 229, 233, 239, 241, 251]
+img=image.imread("virag.jpg")
+averageint = img.mean()
 def atalakito(kep):
-    red = img[:, :, 2]
-    green = img[:, :, 1]
-    blue = img[:, :, 0]
-    for i in range(red):
-        red += 1
-    for j in range(green):
-        green[j] = random.choice(primls)
-    for k in range(blue):
+
+    width = kep.shape[0]
+    height = kep.shape[1]
+    result = np.zeros((width,height,3),dtype=int)
+    redCurrent = 1
+    for row in range(len(result)):
+        for pixel in range(len(result[row])):
+            if redCurrent > 255:
+                 redCurrent = 1
 
 
 
