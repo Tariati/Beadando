5653cf9a9484e13e89bb8b1fbb472c6ad1fd9e06
Tarcsa Attila <79060616+Tariati@users.noreply.github.com>
2021-04-29 11:54:25 +0200

    Add files via upload

--- 1.feladat.py
+++ 1.feladat.py
@@ -4,27 +4,38 @@ def szorasszamolo(ls):
     db = 0
     for i in range(len(ls)):
         db += 1
-        sum = sum + ls[i]
+        sum = sum + int(ls[i])
     atlag = int(sum/db)
     for j in range(len(ls)):
-        x = (atlag - ls[j])**2
+        x = (atlag - int(ls[j]))**2
         szoras += x
     result = (szoras/db)**0.5
     return result
 
-n = int(input("Adjon meg egy számot:"))
-parosls = []
-paratlanls = []
-for i in range(n):
-    n2 = input("Adjon meg számokat,szóközzel elválasztva:")
-    reszek = n2.split(" ")
-    for j in range(len(reszek)):
-        if reszek[j] % 2 == 0:
-            parosls.append(reszek[j])
-        else:
-            paratlanls.append(reszek[j])
-print("Páros számok szórása:",szorasszamolo(parosls),"Páratlan számok szórása:",szorasszamolo(paratlanls))
 
+def helyiertekszamolo(ls):
+    result = 0
+    for i in range(len(ls)):
+        ls[i] = str(ls[i])[:1]
+        result += int(ls[i])
+    return result
+
+x = 0
+n = int(input("Adjon meg egy számot:"))
+for i in range(n):
+    parosls = []
+    paratlanls = []
+    n2 = input("Adjon meg számokat,szóközzel elválasztva:").split(" ")
+    for j in range(len(n2)):
+        if int(n2[j]) % 2 == 0:
+            parosls.append(n2[j])
+        else:
+            paratlanls.append(n2[j])
+    x += 1
+    print(parosls,paratlanls)
+    print("{}.sor".format(x))
+    print("Páros számok szórása:",szorasszamolo(parosls),"Páratlan számok szórása:",szorasszamolo(paratlanls))
+    print("Számjegyek összege(legnagyobb helyiértékek):",helyiertekszamolo(n2))
 
 
 
