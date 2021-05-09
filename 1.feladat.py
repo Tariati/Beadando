ca3cb2aedb857bdbbff900e1d0b9e36c718f8192
Tarcsa Attila <79060616+Tariati@users.noreply.github.com>
2021-05-09 10:22:40 +0200

    Add files via upload

--- 1.feladat.py
+++ 1.feladat.py
@@ -2,43 +2,63 @@ def szorasszamolo(ls):
     szoras = 0
     sum = 0
     db = 0
+    atlag = 0
     for i in range(len(ls)):
         db += 1
         sum = sum + int(ls[i])
-    atlag = int(sum/db)
+    try:
+        atlag = int(sum/db)
+    except ZeroDivisionError:
+        print("A szórás nem számolható!")
     for j in range(len(ls)):
         x = (atlag - int(ls[j]))**2
         szoras += x
-    result = (szoras/db)**0.5
-    return result
-
+    try:
+        return (szoras/db)**0.5
+    except ZeroDivisionError:
+        print("A szórás nem számolható!")
+        exit(-3)
 
 def helyiertekszamolo(ls):
     result = 0
     for i in range(len(ls)):
         firstChar = str(ls[i])[:1]
         if firstChar == "-":
-            result += int(str(ls[i][1]))
+            result += int(str(ls[i])[1])
         else:
             result += int(firstChar)
     return result
 
+def tesztesetek():
+    try:
+        return int(input("Adjon meg egy számot:"))
+    except ValueError:
+        print("Számot adjon meg!")
+        exit(-1)
+
+def szamsplit():
+    n2 = input("Adjon meg számokat,szóközzel elválasztva:").split(" ")
+    result = []
+    try:
+        for i in n2:
+            result.append(int(i))
+        return result
+    except ValueError:
+        print("Számokat adjon meg egy szóközzel elválasztva!")
+        exit(-2)
+
 x = 0
-n = int(input("Adjon meg egy számot:"))
+n = tesztesetek()
 for i in range(n):
     parosls = []
     paratlanls = []
-    n2 = input("Adjon meg számokat,szóközzel elválasztva:").split(" ")
-    for j in range(len(n2)):
-        if int(n2[j]) % 2 == 0:
-            parosls.append(n2[j])
+    n2 = szamsplit()
+    for j in n2:
+        if j % 2 == 0:
+            parosls.append(j)
         else:
-            paratlanls.append(n2[j])
+            paratlanls.append(j)
     x += 1
     print("{}.sor".format(x))
-    print("Páros számok szórása:",szorasszamolo(parosls),"Páratlan számok szórása:",szorasszamolo(paratlanls))
+    print("Páros számok szórása:","%.2f" % szorasszamolo(parosls),"Páratlan számok szórása:","%.2f" % szorasszamolo(paratlanls))
     print("Számjegyek összege(legnagyobb helyiértékek):",helyiertekszamolo(n2))
-
-
-
-
